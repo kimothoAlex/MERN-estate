ef9d505e8951f0a94aa28fbf2f2f01dd79505243
Alex <muchenealex24@gmail.com>
2023-10-10 08:02:44 +0300

    Create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,9 @@
 import bcryptjs from "bcryptjs";
 import User from "../models/user.model.js";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import { errorHandler } from "../utils/error.js";
+dotenv.config();
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
 
@@ -9,6 +13,24 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(200).json("User created successfully");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "Invalid user"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(404, "Wrong credentials"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .cookie("access token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error);
   }
