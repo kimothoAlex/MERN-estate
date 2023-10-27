9c75ab8a82bc37e68b4dcd2d32d6494b12151ff6
Alex <muchenealex24@gmail.com>
2023-10-27 23:43:38 +0300

    complete update listing api

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,9 +1,14 @@
 import express from "express";
 import { verifyToken } from "../utils/verifyUser.js";
-import { createListing } from "../controllers/listing.controller.js";
+import {
+  createListing,
+  deleteListing,
+  updateListing,
+} from "../controllers/listing.controller.js";
 
 const router = express.Router();
 
 router.post("/create", verifyToken, createListing);
-
+router.delete("/delete", verifyToken, deleteListing);
+router.post("/update", verifyToken, updateListing);
 export default router;
